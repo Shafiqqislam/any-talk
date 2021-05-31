d33394c54bc8f2c0d91869a62ae3ffd0294ffe7a
Md-Kais <mdkais3@gmail.com>
2021-06-01 01:36:25 +0600

    ui-complete

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,43 +1,89 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar,makeStyles,IconButton } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setChat } from './features/chatSlice';
 import db from './features/firebase';
 import "./SidebarChat.css";
 import * as timeago from 'timeago.js';
+const useStyles = makeStyles({
+    root: {
+        "&:hover": {
 
-const SidebarChat = ({id,chatName}) => {
+            borderRadius: '20%'
+        }
+    }
+});
+const SidebarChat = ({ id, chatName }) => {
     const dispatch = useDispatch();
-    const [chatInfo,setChatInfo]=useState([]);
+    const [chatInfo, setChatInfo] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         db.collection("chats")
-        .doc(id)
-        .collection("messages")
-        .orderBy("timestamp","desc")
-        .onSnapshot((snapshot)=>
-        setChatInfo(snapshot.docs.map((doc)=>doc.data()))
-        );
-    },[id]);
+            .doc(id)
+            .collection("messages")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) =>
+                setChatInfo(snapshot.docs.map((doc) => doc.data()))
+            );
+    }, [id]);
     return (
         <div
-        onClick={()=>
-        dispatch(
-            setChat({
-                chatId: id,
-                chatName: chatName,
-            })
-        )
-    }
-        className="sidebarChat">
-            <Avatar src={chatInfo[0]?.photo}/>
+            onClick={() =>
+                dispatch(
+                    setChat({
+                        chatId: id,
+                        chatName: chatName,
+                    })
+                )
+            }
+            className="sidebarChat">
+            <IconButton>
+                <Avatar src={chatInfo[0]?.photo} />
+            </IconButton>
             <div className="sidebarChat__info">
                 <h3>{chatName}</h3>
                 <p>{chatInfo[0]?.message}</p>
                 <small>{timeago.format(new Date(chatInfo[0]?.timestamp?.toDate()))}</small>
             </div>
         </div>
+
+
     );
 };
+
+
+// const SidebarChat = ({id,chatName}) => {
+//     const dispatch = useDispatch();
+//     const [chatInfo,setChatInfo]=useState([]);
+
+//     useEffect(()=>{
+//         db.collection("chats")
+//         .doc(id)
+//         .collection("messages")
+//         .orderBy("timestamp","desc")
+//         .onSnapshot((snapshot)=>
+//         setChatInfo(snapshot.docs.map((doc)=>doc.data()))
+//         );
+//     },[id]);
+//     return (
+//         <div
+//         onClick={()=>
+//         dispatch(
+//             setChat({
+//                 chatId: id,
+//                 chatName: chatName,
+//             })
+//         )
+//     }
+//         className="sidebarChat">
+//             <Avatar src={chatInfo[0]?.photo}/>
+//             <div className="sidebarChat__info">
+//                 <h3>{chatName}</h3>
+//                 <p>{chatInfo[0]?.message}</p>
+//                 <small>{timeago.format(new Date(chatInfo[0]?.timestamp?.toDate()))}</small>
+//             </div>
+//         </div>
+//     );
+// };
 
 export default SidebarChat;
